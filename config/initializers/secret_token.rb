97bfbd7804b7e39aae48cc817633d6a296ed034e
root <root@mj-Ideapad-Z560.(none)>
2012-07-03 15:46:36 +0530

    first_commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Infoapp::Application.config.secret_token = '3d561dff529d90a32e1fa9c4fd798d7b8273304d1794c5677f176fa6bcfa2e037d7607bfa33d7f26732a42d88ec8a693f6d9e3b5d3d8d247e750336257edbec3'
